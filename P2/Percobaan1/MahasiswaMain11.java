71d5bc3351adf2c13aa0c7ca30834c9b4e32dd39
I Gusti Agung Timothy Pasquale Anggra Putra <timothypap18@gmail.com>
2026-03-01 17:45:02 +0700

    Manambahkan file

---
+++ P2/Percobaan1/MahasiswaMain11.java
@@ -0,0 +1,16 @@
+package P2.Percobaan1;
+
+public class MahasiswaMain11 {
+    public static void main(String[] args) {
+        Mahasiswa11 mhs1 = new Mahasiswa11();
+        mhs1.nama = "I Gusti Agung Timothy Pasquale Anggra Putra";
+        mhs1.nim = "254107020157";
+        mhs1.kelas = "TI-1A";
+        mhs1.ipk = 3.55;
+        mhs1.tampilkaninfo();
+        mhs1.ubahkelas("TI-1B");
+        mhs1.ubahipk(3.69);
+        mhs1.tampilkaninfo();
+
+    }
+}
