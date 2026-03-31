44bd0a14262012825c2754e033d08579779f69e2
I Gusti Agung Timothy Pasquale Anggra Putra <timothypap18@gmail.com>
2026-03-31 14:33:18 +0700

    P6_Percobaan2_ShortingMain10

---
+++ P6/Praktikum1/SortingMain10.java
@@ -0,0 +1,31 @@
+package P6.Praktikum1;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class SortingMain10 {
+    public static void main(String[] args) {
+        int a[] = {20, 10, 2, 7, 12};
+        Sorting10 dataurut1 = new Sorting10 (a, a.length);
+        System.out.println("Data Awal 1");
+        dataurut1.tampil();
+        dataurut1.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC )");
+        dataurut1.tampil();
+
+        int b[] = {30, 20, 2, 8, 14};
+        Sorting10 dataurut2 = new Sorting10 (b, b.length);
+        System.out.println("Data Awal 2");
+        dataurut2.tampil();
+        dataurut2.SelectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC )");
+        dataurut2.tampil();
+
+        int c[] = {40, 10, 4, 9, 3};
+        Sorting10 dataurut3 = new Sorting10 (c, c.length);
+        System.out.println("Data Awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC )");
+        dataurut3.tampil();
+    }
+}
