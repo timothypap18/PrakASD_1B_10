28fc3369c74ac838f82656163962a9560b78c647
I Gusti Agung Timothy Pasquale Anggra Putra <timothypap18@gmail.com>
2026-06-28 22:13:11 +0700

    Tugas4

--- P10/Prak2/AntrianLayanan.java
+++ P10/Prak2/AntrianLayanan.java
@@ -69,6 +69,16 @@ public class AntrianLayanan {
         }
     }
 
+    public void lihatAkhir() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            System.out.print("Mahasiswa paling belakang: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[rear].tampilkanData();
+        }
+    }
+
     public int getJumlahAntrian() {
         return size;
     }
